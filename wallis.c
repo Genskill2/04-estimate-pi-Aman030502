e211de0b6cbe0151458ef98d813aea0cff1ca9e0
Aman030502 <84667922+Aman030502@users.noreply.github.com>
2021-06-04 10:59:39 +0400

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -28,12 +28,11 @@ int main(void) {
 #include <stdio.h>
 #include <math.h>
 
+
 float wallis_pi(int n)
-{	
-  float pi;
+ {	float pi;
  	float prod=1;
- 	for(int i=1;i<=n;i++)
-  {
+ 	for(int i=1;i<=n;i++){
  	float m = (4.0*i*i)/((4.0*i*i)-1);
  	prod=prod*m;
  	}
@@ -41,31 +40,26 @@ float wallis_pi(int n)
  	return pi;
 }
 
-int main(void) 
-{
+
+
+int main(void) {
   float pi;
-  for (int i=0; i<5; i++) 
-  {
+  for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) 
-    {
+    if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
   }
 
-  for (int i=500; i<3000; i++) 
-  {
+  for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) 
-    {
+    if (!(fabs(pi - M_PI) < 0.01)) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
       abort();
     }
   }
 }
-
-
 
 
   
